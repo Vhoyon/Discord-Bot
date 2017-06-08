bd014a98fbe2307ba725893422376e379033067d
Stephano <Stephano@192.168.2.18>
2017-06-07 20:01:46 -0400

    Meilleur organisation et séparation du code des commandes dans des classes séparé

---
+++ Discord Bot/src/Commands/JoinVoiceChannel.java
@@ -0,0 +1,35 @@
+package Commands;
+
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.VoiceChannel;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.managers.ChannelManager;
+import net.dv8tion.jda.core.managers.GuildManager;
+
+/**
+ * 
+ * @author Stephano
+ *<br><br>
+ *
+ *Classe qui permet au bot de se connecter au serveur local ou se trouve l'utilisateur
+ */
+
+public class JoinVoiceChannel {
+
+	public JoinVoiceChannel(MessageReceivedEvent event) {
+		VoiceChannel vc = null;
+		GuildManager gm = null;
+		for (VoiceChannel channel : event.getGuild().getVoiceChannels()) {
+			vc = channel;
+			for (Member usr : vc.getMembers()) {
+				if (usr.getEffectiveName().equals(event.getAuthor().getName())) {
+					gm = new GuildManager(event.getGuild());
+					ChannelManager cm = vc.getManager();
+					break;
+				}
+			}
+
+		}
+	}
+
+}
--- Discord Bot/src/MessageListener.java
+++ Discord Bot/src/MessageListener.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import Commands.Help;
+import Commands.JoinVoiceChannel;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.audio.AudioReceiveHandler;
 import net.dv8tion.jda.core.audio.AudioSendHandler;
@@ -21,195 +23,44 @@ import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.entities.VoiceChannel;
 
-public class MessageListener extends ListenerAdapter implements AudioManager{
-	
-	
-	 @Override
-	    public void onMessageReceived(MessageReceivedEvent event)
-	    {
-		   Message msg = event.getMessage();
-	       String messageRecu = event.getMessage().getContent();
-	       TextChannel textChannel =  event.getTextChannel();
-	        if(messageRecu.contains("!")){
-	        	if(messageRecu.equalsIgnoreCase("!hello")){
-	        		textChannel.sendMessage("hello " + event.getAuthor().getName()).complete();
-	        	}else if(messageRecu.equalsIgnoreCase("!help")){
-	        		
-	        		event.getAuthor().openPrivateChannel();
-	        		if(event.getAuthor().hasPrivateChannel()){
-	        			event.getAuthor().getPrivateChannel().sendMessage("Help is here").complete();
-	        		}
-	        	}else if(messageRecu.equalsIgnoreCase("!connect")){
-	                VoiceChannel vc = null;
-	                GuildManager gm = null;
-	                for (VoiceChannel channel : event.getGuild().getVoiceChannels()) {
-	                    vc = channel;
-	                    for (Member usr: vc.getMembers()) {
-	                        if (usr.getEffectiveName().equals(event.getAuthor().getName())){
-	                            gm = new GuildManager(event.getGuild());
-	                            ChannelManager cm = vc.getManager();
-	                            break;
-	                        }
-	                    }
-	                    
-	                }
-	                
-	            }else if(messageRecu.contains("!play ") ){
-	        		String playLink = messageRecu.substring(6);
-	        		textChannel.sendMessage(playLink).complete();
-	            
-	        	}
-	        	else if(messageRecu.equalsIgnoreCase("!clear") ){
-	        		List<VoiceChannel> voiceChanel = event.getAuthor().getJDA().getVoiceChannels();
-	        		for (VoiceChannel voiceChannel : voiceChanel) {
-	        			List<Member> members = voiceChannel.getMembers();
-	        			for (Member member : members) {
-						}
+
+/**
+ * 
+ * @author Stephano
+ *
+ *<br><br>Cette classe extend <b>ListenerAdapter</b> recoit les commandes de l'utilisateur et appele les classes necessaires
+ */
+public class MessageListener extends ListenerAdapter{
+
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
+		Message msg = event.getMessage();
+		String messageRecu = event.getMessage().getContent();
+		TextChannel textChannel = event.getTextChannel();
+		if (messageRecu.contains("!")) {
+			if (messageRecu.equalsIgnoreCase("!hello")) {
+				textChannel.sendMessage("hello " + event.getAuthor().getName()).complete();
+			} else if (messageRecu.equalsIgnoreCase("!help")) {
+				Help help = new Help(event);
+			} else if (messageRecu.equalsIgnoreCase("!connect")) {
+				JoinVoiceChannel join = new JoinVoiceChannel(event);
+
+			} else if (messageRecu.contains("!play ")) {
+				String playLink = messageRecu.substring(6);
+				textChannel.sendMessage(playLink).complete();
+
+			} else if (messageRecu.equalsIgnoreCase("!clear")) {
+				List<VoiceChannel> voiceChanel = event.getAuthor().getJDA().getVoiceChannels();
+				for (VoiceChannel voiceChannel : voiceChanel) {
+					List<Member> members = voiceChannel.getMembers();
+					for (Member member : members) {
 					}
-//	        		textChannel.deleteMessages((Collection<Message>) event.getTextChannel().getHistory().getRetrievedHistory().subList(0, textChannel.getHistory().size() + 1)).complete();
-	            
-	        	}
-	        }
-	    }
+				}
+				// textChannel.deleteMessages((Collection<Message>)
+				// event.getTextChannel().getHistory().getRetrievedHistory().subList(0,
+				// textChannel.getHistory().size() + 1)).complete();
 
-	@Override
-	public void closeAudioConnection() {
-		// TODO Auto-generated method stub
-		
+			}
+		}
 	}
-
-	@Override
-	public long getConnectTimeout() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public VoiceChannel getConnectedChannel() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ConnectionListener getConnectionListener() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ConnectionStatus getConnectionStatus() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Guild getGuild() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public JDA getJDA() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public VoiceChannel getQueuedAudioConnection() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public AudioReceiveHandler getReceiveHandler() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public AudioSendHandler getSendingHandler() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isAttemptingToConnect() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isAutoReconnect() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isConnected() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isSelfDeafened() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isSelfMuted() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void openAudioConnection(VoiceChannel arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setAutoReconnect(boolean arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setConnectTimeout(long arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setConnectionListener(ConnectionListener arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setReceivingHandler(AudioReceiveHandler arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setSelfDeafened(boolean arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setSelfMuted(boolean arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setSendingHandler(AudioSendHandler arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	 
-	 
-
 }
