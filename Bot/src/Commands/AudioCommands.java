dd7fb8781009d3e51cf637ed0caec79970076864
StephanoMehawej <stephanomehawej10@hotmail.com>
2017-06-12 10:55:54 -0400

    push

---
+++ Bot/src/Commands/AudioCommands.java
@@ -0,0 +1,44 @@
+package Commands;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.spi.AudioFileReader;
+
+import net.dv8tion.jda.core.audio.AudioPacket;
+import net.dv8tion.jda.core.entities.AudioChannel;
+import net.dv8tion.jda.core.entities.impl.GuildImpl;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.managers.AudioManager;
+import net.dv8tion.jda.core.managers.impl.AudioManagerImpl;
+
+/**
+ * 
+ * @author Stephano
+ *
+ *Classe qui contiens tout les methodes qui font la getion de l'audio
+ *
+ *<b>NE FONCTIONNE PAS</b>
+ */
+public class AudioCommands {
+	private MessageReceivedEvent events;
+	private String link;
+	public AudioCommands(MessageReceivedEvent event) {
+		events = event;
+		
+	}
+	
+	public void play(){
+		link = events.getMessage().getContent().substring(6);
+		
+		
+
+	}
+
+}
---
+++ Bot/src/Commands/Clear.java
@@ -0,0 +1,39 @@
+package Commands;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageHistory;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.VoiceChannel;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.requests.RestAction;
+
+
+/**
+ * 
+ * @author Stephano
+ *
+ *Classe qui permet d'éffacer tout les messages dans le chat ou on l'invoque
+ *
+ */
+public class Clear {
+
+	public Clear(MessageReceivedEvent event) {
+		boolean vide = false;
+		MessageHistory hist = event.getTextChannel().getHistory();
+		do{
+		List<Message> messages = hist.retrievePast(100).complete();
+		if(!messages.isEmpty()){
+		event.getTextChannel().deleteMessages(messages).complete();
+		}else{
+			vide = true;
+		}
+		}while(!vide);
+
+	}
+
+}
