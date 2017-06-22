68078604e6fb031195796f54b43f51ca90e384c4
Stephano <Stephano@192.168.2.18>
2017-06-21 21:30:38 -0400

    testing stuff

--- Discord Bot/src/MessageListener.java
+++ Discord Bot/src/MessageListener.java
@@ -36,7 +36,6 @@ public class MessageListener extends ListenerAdapter {
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event) {
 		AudioCommands audio = new AudioCommands(event);
-		Message msg = event.getMessage();
 		String messageRecu = event.getMessage().getContent();
 		TextChannel textChannel = event.getTextChannel();
 		VoiceChannelInteraction voiceChannels = new VoiceChannelInteraction(event);
