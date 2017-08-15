6bdb21c3d9d120ce329498628eb0335cab4252e7
StephanoMehawej <stephanomehawej10@hotmail.com>
2017-08-15 19:59:06 -0400

    refactor

---
+++ Discord Bot/src/Commands/Game.java
@@ -0,0 +1,16 @@
+package Commands;
+
+import java.util.Random;
+
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class Game {
+
+	public Game(MessageReceivedEvent event, String messageRecu) {
+	  String[] jeux = messageRecu.split(",");
+	 Random ran = new Random();
+	 int num = ran.nextInt(jeux.length);
+	 event.getTextChannel().sendMessage(jeux[num]).complete();
+	}
+
+}
