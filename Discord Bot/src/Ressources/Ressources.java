3f4c7d932fb20d1bd0d5e8990331457802a05ff2
StephanoMehawej <stephanomehawej10@hotmail.com>
2017-08-15 20:03:17 -0400

    google api

---
+++ Discord Bot/src/Ressources/Ressources.java
@@ -0,0 +1,13 @@
+package Ressources;
+/**
+ * 
+ * @author Stephano
+ *
+ *         Contient les constantes du bot
+ *
+ */
+public class Ressources {
+	final public static String DISCORDTOKEN = "MzIxMTQ1NDI0NTQ5MzgwMDk3.DBZxmA.CyEXFP8cQEkV5Qn8X-PKLPY4EhM";
+	final public static String YOUTUBETOKEN = "AIzaSyDfPTuiyimq09IK5umHfD4OY8OV5oya2mU";
+
+}
