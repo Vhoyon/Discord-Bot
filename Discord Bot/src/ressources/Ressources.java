ff7315b1bdecdb2178fde078c96eb1302ab6ae7a
V-ed <guillaumemarcoux@gmail.com>
2017-08-17 23:03:52 -0400

    Most crucial commit. Trust me. Look at the changes.

--- Discord Bot/src/ressources/Ressources.java
+++ Discord Bot/src/ressources/Ressources.java
@@ -11,6 +11,6 @@ public class Ressources {
 	final public static String DISCORDTOKEN = "MzIxMTQ1NDI0NTQ5MzgwMDk3.DBZxmA.CyEXFP8cQEkV5Qn8X-PKLPY4EhM";
 	final public static String YOUTUBETOKEN = "AIzaSyDfPTuiyimq09IK5umHfD4OY8OV5oya2mU";
 	
-	final public static String PREFIX = "!";
+	final public static String PREFIX = "!!";
 
 }
