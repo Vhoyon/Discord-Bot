8747455be5e9ae6cd20c979e34380a9792278852
StephanoMehawej <stephanomehawej10@hotmail.com>
2017-09-02 14:19:37 -0400

    Timer kind of works but still a lot of work to do

--- Discord Bot/src/commands/CommandTimer.java
+++ Discord Bot/src/commands/CommandTimer.java
@@ -1,18 +1,40 @@
 package commands;
 
+import framework.Buffer;
 import framework.Command;
 
 public class CommandTimer extends Command {
 	
 	@Override
 	public void action(){
-		int seconds = 10;
+		String[] constraints = getSplitContent();
+		int seconds = 0;
+		int count = 0;
+		Boolean timerOn = true;
+		try{
+			seconds = Integer.parseInt(constraints[0]);
+			
+		}
+		catch(NullPointerException e){
+			sendMessage("You must give an amount of time to the "
+					+ buildVCommand("timer") + " command for it too count");
+		}
+		
+		String lastmsgId = null;
+		long totalTime = (seconds * 1000);
 		long temps = System.currentTimeMillis();
-//		while(System.currentTimeMillis()-temps != seconds*1000){
-//			String messageToSend = String.valueOf(System.currentTimeMillis()-temps == seconds);
-//		}
-		sendMessage("LOL");
-		editMessage("IT WORKS", (String)getBuffer().get(BUFFER_LASTMSGID));
+		sendMessage(seconds + " seconds");
+		lastmsgId = getTextContext().getLatestMessageId();
+		for(int i = seconds; i >= 0; i--){
+			editMessage(i + " seconds", lastmsgId);
+			if(i == 0){
+				sendMessage("TimerEnded");
+			}
+			try{
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException e){}
+		}
 		
 	}
 	
