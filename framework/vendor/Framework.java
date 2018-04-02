a318676cea4bce26004c540c4bdef7aa7802b8bb
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-04-02 12:26:58 -0400

    Add Framework static values to use throughout the whole app

--- framework/vendor/Framework.java
+++ framework/vendor/Framework.java
@@ -2,17 +2,33 @@ package vendor;
 
 import vendor.abstracts.Module;
 
+import java.awt.*;
+import java.io.*;
+import java.net.URLDecoder;
+
 public class Framework {
+	
+	private static boolean IS_RUNNING_FROM_TERMINAL;
+	private static String RUNNABLE_SYSTEM_PATH;
 	
 	private Framework(){}
 	
 	private static String[] modules =
 	{
-		"Environment",
-		"Logger"
+		"Environment", "Logger"
 	};
 	
+	public static boolean isRunningFromTerminal(){
+		return IS_RUNNING_FROM_TERMINAL;
+	}
+
+	public static String runnableSystemPath(){
+		return RUNNABLE_SYSTEM_PATH;
+	}
+	
 	public static void build() throws Exception{
+		
+		Framework.setupGlobalVariables();
 		
 		StringBuilder errors = new StringBuilder();
 		
@@ -27,7 +43,7 @@ public class Framework {
 				if(Module.class.isAssignableFrom(moduleClass)){
 					
 					Module module = (Module)moduleClass.newInstance();
-
+					
 					try{
 						module.build();
 					}
@@ -50,6 +66,37 @@ public class Framework {
 		if(errors.length() != 0){
 			throw new Exception(errors.toString());
 		}
+		
+	}
+	
+	private static void setupGlobalVariables(){
+		
+		IS_RUNNING_FROM_TERMINAL = getIsRunningFromTerminal();
+		RUNNABLE_SYSTEM_PATH = getRunnableFolderPath();
+		
+	}
+	
+	private static boolean getIsRunningFromTerminal(){
+		Console console = System.console();
+		
+		return !(console == null && !GraphicsEnvironment.isHeadless());
+	}
+	
+	private static String getRunnableFolderPath(){
+		
+		String systemPath = Framework.class.getProtectionDomain()
+				.getCodeSource().getLocation().getPath();
+		
+		try{
+			systemPath = URLDecoder.decode(systemPath, "UTF-8");
+		}
+		catch(UnsupportedEncodingException e){}
+		
+		File runner = new File(systemPath);
+		
+		String decoratedSystemPath = runner.getParent() + File.separator;
+		
+		return decoratedSystemPath;
 		
 	}
 	
