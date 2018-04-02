b93572bbe5e8342c82123a5570d4f3588fa64ac1
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-04-02 16:43:21 -0400

    Extract Notification logger out to be usable for other components

--- framework/vendor/ui/NotificationUI.java
+++ framework/vendor/ui/NotificationUI.java
@@ -1,35 +1,21 @@
 package vendor.ui;
 
 import vendor.abstracts.AbstractUIConsole;
-import vendor.interfaces.Loggable;
 import vendor.modules.Logger;
+import vendor.objects.NotificationLogger;
 
-import javax.swing.*;
 
 public class NotificationUI extends AbstractUIConsole {
-	
+
+	private NotificationLogger logger;
+
 	@Override
 	public void initialize(){
-		Logger.setOutputs(new NotificationLogger(this));
+
+		logger = new NotificationLogger(this);
+
+		Logger.setOutputs(logger);
+
 	}
-	
-	@Override
-	public int getConfirmation(String question, QuestionType questionType){
-		return super.getConfirmation(question, questionType);
-	}
-	
-	private class NotificationLogger implements Loggable {
-		
-		private JFrame frame;
-		
-		public NotificationLogger(JFrame frame){
-			this.frame = frame;
-		}
-		
-		@Override
-		public void log(String logText, String logType, boolean hasAppendedDate){
-			JOptionPane.showMessageDialog(frame, logText);
-		}
-	}
-	
+
 }
