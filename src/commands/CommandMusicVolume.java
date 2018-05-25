b0175cdfc54fc92605ddd6dbfcba6e2d135f7dd0
Stephano <stephanomehawej10@hotmail.com>
2018-05-24 20:35:16 -0400

    Optimise the volume changing
    Add setVolume to MusicPlayer

--- src/commands/CommandMusicVolume.java
+++ src/commands/CommandMusicVolume.java
@@ -8,27 +8,19 @@ import vendor.exceptions.BadParameterException;
 import errorHandling.BotError;
 
 public class CommandMusicVolume extends MusicCommands {
-	
+
 	@Override
 	public void action(){
-		
+
 		String content = getContent();
-		
+
 		try{
-			
+
 			int volume = Integer.valueOf(content);
-			
-			if(volume < 0 || volume > 100){
-				throw new BadParameterException();
-			}
-			else{
-				
-				MusicManager.get().getPlayer(getGuild()).getAudioPlayer()
-						.setVolume(volume / (100 / MusicPlayer.MAX_VOLUME));
-				
-				sendMessage(lang("ChangedSuccess", volume));
-				
-			}
+
+			MusicManager.get().getPlayer(getGuild()).setVolume(volume);
+
+			sendMessage(lang("ChangedSuccess", volume));
 			
 		}
 		catch(NumberFormatException e){
@@ -37,9 +29,9 @@ public class CommandMusicVolume extends MusicCommands {
 		catch(BadParameterException e){
 			new BotError(this, lang("NumberNotBetweenRange", 0, 100));
 		}
-		
+
 	}
-	
+
 	@Override
 	public Object getCalls(){
 		return MUSIC_VOLUME;
--- src/utilities/music/MusicPlayer.java
+++ src/utilities/music/MusicPlayer.java
@@ -5,11 +5,15 @@ import net.dv8tion.jda.core.entities.VoiceChannel;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import vendor.exceptions.BadParameterException;
+import vendor.modules.Environment;
+import vendor.modules.Logger;
 
 public class MusicPlayer {
 	
 	public final static int MAX_VOLUME = 20;
-	
+	public final static int DEFAULT_VOLUME= 75;
+
 	private final AudioPlayer audioPlayer;
 	private final AudioListener listener;
 	private final Guild guild;
@@ -17,10 +21,16 @@ public class MusicPlayer {
 	public MusicPlayer(AudioPlayer audioPlayer, Guild guild){
 		this.audioPlayer = audioPlayer;
 		this.guild = guild;
-		
-		// Defaults volume to 75% of our maxed value (20) by default
-		this.audioPlayer.setVolume(15);
-		
+
+//		 Defaults volume to 75% of our maxed value (20) by default
+		int defaultVolume  = Environment.getVar("DEFAULT_VOLUME",75);
+
+		try {
+			this.setVolume(defaultVolume);
+		} catch (BadParameterException e) {
+			Logger.log(e);
+		}
+
 		listener = new AudioListener(this);
 		audioPlayer.addListener(listener);
 	}
@@ -76,5 +86,19 @@ public class MusicPlayer {
 	public void closeConnection(){
 		this.getGuild().getAudioManager().closeAudioConnection();
 	}
+
+	public void setVolume(int volume) throws BadParameterException{
+
+
+        if(volume < 0 || volume > 100){
+        	setVolume(DEFAULT_VOLUME);
+            throw new BadParameterException("Volume must be between 0 and 100.");
+        }
+        else{
+
+            this.getAudioPlayer().setVolume(volume / (100 / MAX_VOLUME));
+
+        }
+    }
 	
 }
