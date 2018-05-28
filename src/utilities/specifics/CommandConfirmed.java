9da1089194c69fb49c7ad869cbedb1c568afc7fb
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-05-27 22:16:28 -0400

    Tweak CommandConfirmed class to use the new AbstractCommandConfirmed
    This commit was the last step for issue #52.
    Closes #52

--- src/utilities/specifics/CommandConfirmed.java
+++ src/utilities/specifics/CommandConfirmed.java
@@ -1,71 +1,30 @@
 package utilities.specifics;
 
 import utilities.BotCommand;
+import utilities.interfaces.Commands;
+import vendor.abstracts.AbstractCommandConfirmed;
 
-public abstract class CommandConfirmed extends BotCommand {
-	
-	private BotCommand inceptionCommand = null;
+public abstract class CommandConfirmed extends AbstractCommandConfirmed
+		implements Commands {
 	
 	public CommandConfirmed(){}
 	
 	public CommandConfirmed(BotCommand inceptionCommand){
 		super(inceptionCommand);
-		
-		this.inceptionCommand = inceptionCommand;
-		
-		action();
 	}
-	
-	public abstract String getConfMessage();
 	
 	@Override
-	public void action(){
-		
-		if(!hasMemory(BUFFER_CONFIRMATION)){
-			//			getMemory(BUFFER_CONFIRMATION);
-			//		}
-			//		else{
-			
-			sendInfoMessage(
-					getConfMessage()
-							+ "\n\n"
-							+ lang("CommandConfirmedCustomAndConfirmMessage",
-									buildVCommand(CONFIRM),
-									buildVCommand(CANCEL)), false);
-			
-			remember(this, BUFFER_CONFIRMATION);
-			
-		}
-		
+	protected void actionIfConfirmable(){
+		sendInfoMessage(
+				getConfMessage()
+						+ "\n\n"
+						+ lang("CommandConfirmedCustomAndConfirmMessage",
+								buildVCommand(CONFIRM), buildVCommand(CANCEL)),
+				false);
 	}
-	
-	public abstract void confirmed();
 	
 	public void cancelled(){
 		sendInfoMessage("*" + lang(true, "CommandConfirmedConfCancelled") + "*");
 	}
 	
-	@Override
-	public String lang(String shortKey){
-		if(inceptionCommand != null){
-			return inceptionCommand.lang(shortKey);
-		}
-		return super.lang(shortKey);
-	}
-	
-	@Override
-	public String lang(String shortKey, Object... replacements){
-		if(inceptionCommand != null){
-			return inceptionCommand.lang(shortKey, replacements);
-		}
-		return super.lang(shortKey, replacements);
-	}
-	
-	@Override
-	public Object getCalls(){
-		if(inceptionCommand == null)
-			return null;
-		
-		return inceptionCommand.getCalls();
-	}
 }
