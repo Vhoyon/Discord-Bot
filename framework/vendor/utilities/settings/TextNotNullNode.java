824cedcd04e802b091d392b6a66d9e6d6e6b0a53
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-06-02 18:05:21 -0400

    Add TextNotNullNode that rejects null or empty text settings
    Ref #35

---
+++ framework/vendor/utilities/settings/TextNotNullNode.java
@@ -0,0 +1,22 @@
+package vendor.utilities.settings;
+
+import vendor.objects.Dictionary;
+
+public class TextNotNullNode extends AbstractNode<String> {
+	
+	public TextNotNullNode(String name, String env, String defaultValue,
+			Dictionary dict){
+		super(name, env, defaultValue, dict);
+	}
+	
+	@Override
+	protected String sanitizeValue(String value)
+			throws IllegalArgumentException{
+		if(value == null || value.length() == 0){
+			throw new IllegalArgumentException("Value cannot be empty!");
+		}
+		
+		return value;
+	}
+	
+}
