7d28f1323ef83514a78260cb00ec0def582d92e0
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-06-14 18:27:55 -0400

    Reformat CommandMusicVolume class

--- src/commands/CommandMusicVolume.java
+++ src/commands/CommandMusicVolume.java
@@ -2,20 +2,19 @@ package commands;
 
 import utilities.abstracts.MusicCommands;
 import utilities.music.MusicManager;
-import utilities.music.MusicPlayer;
 import vendor.exceptions.BadParameterException;
 
 import errorHandling.BotError;
 
 public class CommandMusicVolume extends MusicCommands {
-
+	
 	@Override
 	public void action(){
-
+		
 		String content = getContent();
-
+		
 		try{
-
+			
 			int volume = Integer.valueOf(content);
 			
 			MusicManager.get().getPlayer(this).setVolume(volume);
@@ -29,16 +28,17 @@ public class CommandMusicVolume extends MusicCommands {
 		catch(BadParameterException e){
 			new BotError(this, lang("NumberNotBetweenRange", 0, 100));
 		}
-
+		
 	}
-
+	
 	@Override
 	public Object getCalls(){
 		return MUSIC_VOLUME;
 	}
-
+	
 	@Override
-	public String getCommandDescription() {
+	public String getCommandDescription(){
 		return "Change the volume of the music that the bot plays";
 	}
+	
 }
