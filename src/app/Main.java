1616c2dff7dc74412e669be01018c15dcabb91af
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-07-05 09:37:35 -0400

    Add FrameworkTemplate to allow for easier testing

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,22 +1,14 @@
 package app;
 
-import javax.security.auth.login.LoginException;
-
 import consoles.TerminalConsole;
 import consoles.UIConsole;
-import vendor.objects.Buffer;
-import vendor.utilities.CommandsThreadManager;
+import vendor.utilities.FrameworkTemplate;
 import vendor.Framework;
 import vendor.interfaces.Console;
 import vendor.modules.Environment;
 import vendor.modules.Logger;
-import vendor.modules.Logger.LogType;
-import vendor.modules.Metrics;
 import vendor.objects.Dictionary;
 import vendor.objects.Request;
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.JDABuilder;
 
 /**
  * 
@@ -24,10 +16,6 @@ import net.dv8tion.jda.core.JDABuilder;
  *
  */
 public class Main {
-	
-	private static JDA jda;
-	
-	private static String botToken;
 	
 	public static void main(String[] args){
 		
@@ -44,7 +32,7 @@ public class Main {
 			
 			Framework.build();
 			
-			botToken = Environment.getVar("BOT_TOKEN");
+			FrameworkTemplate.botToken = Environment.getVar("BOT_TOKEN");
 			
 			Console console;
 			
@@ -53,12 +41,12 @@ public class Main {
 				console = new TerminalConsole(){
 					@Override
 					public void onStart() throws Exception{
-						startBot(this);
+						FrameworkTemplate.startBot(this, new MessageListener());
 					}
 					
 					@Override
 					public void onStop() throws Exception{
-						stopBot(this);
+						FrameworkTemplate.stopBot(this);
 					}
 					
 					@Override
@@ -73,12 +61,12 @@ public class Main {
 				console = new UIConsole(){
 					@Override
 					public void onStart() throws Exception{
-						startBot(this);
+						FrameworkTemplate.startBot(this, new MessageListener());
 					}
 					
 					@Override
 					public void onStop() throws Exception{
-						stopBot(this);
+						FrameworkTemplate.stopBot(this);
 					}
 					
 					@Override
@@ -97,105 +85,6 @@ public class Main {
 		}
 		catch(Exception e){
 			e.printStackTrace();
-		}
-		
-	}
-	
-	private static void startBot(Console console) throws Exception{
-		
-		boolean success = false;
-		
-		do{
-			
-			Logger.log("Starting the bot...", LogType.INFO);
-			
-			try{
-				
-				jda = new JDABuilder(AccountType.BOT).setToken(botToken)
-						.buildBlocking();
-				jda.addEventListener(new MessageListener());
-				jda.setAutoReconnect(true);
-				
-				Metrics.startClock();
-				Metrics.setJDA(jda);
-				
-				Logger.log("Bot started!", LogType.INFO);
-				
-				success = true;
-				
-			}
-			catch(LoginException e){
-				
-				botToken = console
-						.getInput("The bot token provided is invalid. Please enter a valid token here :");
-				
-				if(botToken == null || botToken.length() == 0)
-					throw e;
-				
-				Logger.log("Application's Bot Token has been set to : "
-						+ botToken, LogType.INFO);
-				
-			}
-			
-		}while(!success);
-		
-	}
-	
-	private static void stopBot(Console console) throws Exception{
-		
-		if(jda != null){
-			
-			Logger.log("Shutting down the bot...", LogType.INFO);
-			
-			boolean canStopBot = true;
-			
-			if(CommandsThreadManager.hasRunningCommands()){
-				
-				int conf = console
-						.getConfirmation(
-								"There are running commands, are you sure you want to stop the bot?",
-								Console.QuestionType.YES_NO);
-				
-				if(conf == Console.NO){
-					canStopBot = false;
-				}
-				else{
-					
-					int numberOfStoppedCommands = CommandsThreadManager
-							.stopAllCommands();
-					
-					Logger.log("Stopped " + numberOfStoppedCommands
-							+ " running commands before stopping the bot.",
-							LogType.INFO);
-					
-				}
-				
-			}
-			
-			if(!canStopBot){
-				
-				Logger.log("Bot not stopped.", LogType.INFO);
-				
-			}
-			else{
-				
-				jda.shutdownNow();
-				
-				jda = null;
-				
-				Buffer.get().emptyMemory();
-				
-				Logger.log("Bot has been shutdown!", LogType.INFO);
-				
-				Metrics.stopClock();
-				Metrics.setJDA(null);
-				
-			}
-			
-		}
-		else{
-			Logger.log("The JDA was already closed, no action was taken.",
-					LogType.INFO);
 		}
 		
 	}
