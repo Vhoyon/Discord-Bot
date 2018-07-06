2281e004ad1372834b539cac1732b6f52c2fcfa7
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-07-05 20:28:38 -0400

    Add first test file (testing)

---
+++ src/tests/BotCommandTest.java
@@ -0,0 +1,47 @@
+package tests;
+
+import org.junit.jupiter.api.*;
+
+class BotCommandTest {
+	
+	@BeforeEach
+	void setUp(){
+		
+	}
+	
+	@AfterEach
+	void tearDown(){
+		
+	}
+	
+	@Test
+	void getRouter(){
+		assert false;
+	}
+	
+	@Test
+	void formatParameter(){
+		
+	}
+	
+	@Test
+	void getUsage(){
+		
+	}
+	
+	@Test
+	void setting(){
+		
+	}
+	
+	@Test
+	void setSetting(){
+		
+	}
+	
+	@Test
+	void setSetting1(){
+		
+	}
+	
+}
