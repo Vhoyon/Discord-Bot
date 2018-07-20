4a684a20a57f21e754eca9442e210b7d263a6400
Stephano <stephanomehawej10@hotmail.com>
2018-07-20 09:45:09 -0400

    Allow to search youtube with key words
    For the time being only the first result

--- src/commands/CommandMusicPlay.java
+++ src/commands/CommandMusicPlay.java
@@ -1,5 +1,11 @@
 package commands;
 
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
 import utilities.abstracts.MusicCommands;
 import utilities.music.MusicManager;
 import utilities.music.MusicPlayer;
@@ -7,10 +13,17 @@ import utilities.music.MusicPlayer;
 import net.dv8tion.jda.core.entities.VoiceChannel;
 import errorHandling.BotError;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.google.api.services.youtube.YouTube;
+import vendor.modules.Environment;
+
 public class CommandMusicPlay extends MusicCommands {
 
 	@Override
-	public void action(){
+	public void action() {
 
 		if(getGuild() == null)
 			return;
@@ -35,7 +48,30 @@ public class CommandMusicPlay extends MusicCommands {
 			}
 
 			if(getContent() != null){
-				MusicManager.get().loadTrack(this, getContent());
+				String id;
+				String source;
+				if (!isUrl(getContent())){
+					YouTube youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), new HttpRequestInitializer() {
+						public void initialize(HttpRequest request) throws IOException {
+						}
+					}).setApplicationName("Discord Bot").build();
+					try {
+						YouTube.Search.List search = youtube.search().list("snippet");
+						search.setMaxResults((long) 1);
+						search.setQ(getContent());
+						search.setKey(Environment.getVar("YOUTUBE_TOKEN"));
+						SearchListResponse response = search.execute();
+						id = response.getItems().get(0).getId().getVideoId();
+						System.out.print(id);
+						source = "https://www.youtube.com/watch?v=" + id;
+						MusicManager.get().loadTrack(this, source);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}else{
+					source = getContent();
+					MusicManager.get().loadTrack(this, source);
+				}
 			}
 			else{
 
@@ -64,5 +100,13 @@ public class CommandMusicPlay extends MusicCommands {
 	@Override
 	public String getCommandDescription() {
 		return "Start a song by giving a youtube link or restart a paused song by not giving a link";
+	}
+	public boolean isUrl(String string){
+		try {
+			URL url = new URL(string);
+			return true;
+		} catch (MalformedURLException e) {
+			return false;
+		}
 	}
 }
