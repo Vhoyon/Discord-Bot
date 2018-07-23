c351afee488aca9e444e959430e3749ea08b11c4
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-07-23 00:06:15 -0400

    Add flag for skip command to skip all commands

--- src/commands/CommandMusicSkip.java
+++ src/commands/CommandMusicSkip.java
@@ -7,102 +7,110 @@ import utilities.specifics.CommandConfirmed;
 import vendor.exceptions.BadContentException;
 
 import errorHandling.BotError;
+import vendor.objects.ParametersHelp;
 
 public class CommandMusicSkip extends MusicCommands {
 	
 	@Override
 	public void action(){
 		
-		Boolean canSkipAll = canSkipAll(false);
-		
-		if(canSkipAll != null){
+		if(hasParameter("a")){
+			callCommand(new CommandMusicSkipAll());
+		}
+		else{
 			
-			if(canSkipAll){
+			Boolean canSkipAll = canSkipAll(false);
+			
+			if(canSkipAll != null){
 				
-				new CommandMusicSkipAll().action();
-				
-			}
-			else{
-				
-				MusicPlayer player = MusicManager.get().getPlayer(this);
-				
-				if(getContent() == null){
+				if(canSkipAll){
 					
-					if(!isPlaying()){
-						new BotError(this, lang("NotPlaying"));
-					}
-					else{
-						if(this.hasMemory("MUSIC_LOOP")
-								&& (boolean)this.getMemory("MUSIC_LOOP")){
-							forget("MUSIC_LOOP");
-						}
-						if(player.skipTrack()){
-							sendInfoMessage(lang("SkippedNowPlaying",
-									code(player.getAudioPlayer()
-											.getPlayingTrack().getInfo().title)));
-						}
-						else{
-							sendInfoMessage(lang("NoMoreMusic"));
-							
-							MusicManager.get().emptyPlayer(this);
-						}
-						
-					}
+					new CommandMusicSkipAll().action();
 					
 				}
 				else{
 					
-					try{
+					MusicPlayer player = MusicManager.get().getPlayer(this);
+					
+					if(getContent() == null){
 						
-						int skipAmount = Integer.valueOf(getContent());
-						
-						if(skipAmount < 1){
-							throw new BadContentException();
+						if(!isPlaying()){
+							new BotError(this, lang("NotPlaying"));
 						}
 						else{
-							
-							if(skipAmount <= player.getNumberOfTracks()){
-								
-								for(int i = 0; i < skipAmount; i++){
-									player.skipTrack();
-								}
-								
+							if(this.hasMemory("MUSIC_LOOP")
+									&& (boolean)this.getMemory("MUSIC_LOOP")){
+								forget("MUSIC_LOOP");
+							}
+							if(player.skipTrack()){
 								sendInfoMessage(lang(
 										"SkippedNowPlaying",
-										player.getAudioPlayer()
-												.getPlayingTrack().getInfo().title));
+										code(player.getAudioPlayer()
+												.getPlayingTrack().getInfo().title)));
+							}
+							else{
+								sendInfoMessage(lang("NoMoreMusic"));
 								
+								MusicManager.get().emptyPlayer(this);
+							}
+							
+						}
+						
+					}
+					else{
+						
+						try{
+							
+							int skipAmount = Integer.valueOf(getContent());
+							
+							if(skipAmount < 1){
+								throw new BadContentException();
 							}
 							else{
 								
-								new CommandConfirmed(this){
-									@Override
-									public String getConfMessage(){
-										return lang(
-												"OverflowConfirm",
-												code(skipAmount),
-												code(player.getNumberOfTracks()));
+								if(skipAmount <= player.getNumberOfTracks()){
+									
+									for(int i = 0; i < skipAmount; i++){
+										player.skipTrack();
 									}
 									
-									@Override
-									public void confirmed(){
+									sendInfoMessage(lang("SkippedNowPlaying",
+											player.getAudioPlayer()
+													.getPlayingTrack()
+													.getInfo().title));
+									
+								}
+								else{
+									
+									new CommandConfirmed(this){
+										@Override
+										public String getConfMessage(){
+											return lang(
+													"OverflowConfirm",
+													code(skipAmount),
+													code(player
+															.getNumberOfTracks()));
+										}
 										
-										new CommandMusicSkipAll().action();
-										
-									}
-								};
+										@Override
+										public void confirmed(){
+											callCommand(new CommandMusicSkipAll());
+										}
+									};
+									
+								}
 								
 							}
 							
 						}
+						catch(NumberFormatException e){
+							new BotError(this, lang("NumberNotANumber"));
+						}
+						catch(BadContentException e){
+							new BotError(this, lang("NumberNotBetweenRange", 0,
+									100));
+						}
 						
-					}
-					catch(NumberFormatException e){
-						new BotError(this, lang("NumberNotANumber"));
-					}
-					catch(BadContentException e){
-						new BotError(this,
-								lang("NumberNotBetweenRange", 0, 100));
 					}
 					
 				}
@@ -122,4 +130,15 @@ public class CommandMusicSkip extends MusicCommands {
 	public String getCommandDescription(){
 		return "Skip the song that is currently playing";
 	}
+	
+	@Override
+	public ParametersHelp[] getParametersDescriptions(){
+		return new ParametersHelp[]
+		{
+			new ParametersHelp(
+					"Skips all the commands. Basically does the same as entering "
+							+ buildVCommand("skip-all") + ".", "a", "all")
+		};
+	}
+	
 }
