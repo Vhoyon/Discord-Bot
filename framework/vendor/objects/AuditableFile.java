be0b4fabeeff9db9ebc0271283d72212caaec71d
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-09-19 20:16:32 -0400

    Tweak formatting, error handling and error messages
    Also separated the logic to create the header to modularize this class.

--- framework/vendor/objects/AuditableFile.java
+++ framework/vendor/objects/AuditableFile.java
@@ -1,11 +1,11 @@
 package vendor.objects;
 
-import vendor.exceptions.BadFormatException;
 import vendor.interfaces.Auditable;
 import vendor.modules.Logger;
 import vendor.res.FrameworkResources;
 
 import java.io.*;
+import java.nio.file.Files;
 
 public class AuditableFile implements Auditable {
 	
@@ -28,48 +28,80 @@ public class AuditableFile implements Auditable {
 	public void audit(String auditText, boolean hasAppendedDate,
 			boolean shouldPrependAudit){
 		
+		File auditFile = getFile();
+		
 		try{
-
 			
-			FileWriter writer = new FileWriter(getFile(), true);
-			PrintWriter print_line = new PrintWriter(writer);
-			if (getFile().exists()){
-				FileReader reader = new FileReader(getFile());
-				BufferedReader br = new BufferedReader(reader);
-				String header =  br.readLine();
-				if (header != null){
-					if (!header.equals(FrameworkResources.AUDIT_HEADER)) {
-						throw new BadFormatException();
-					}
-				}else{
-					print_line.println(FrameworkResources.AUDIT_HEADER);
-				}
-				
-			}
-			print_line.println(auditText);
+			PrintWriter writer = handleAuditHeader(auditFile);
 			
-			print_line.close();
+			writer.println(auditText);
 			
+			writer.close();
 			
 		}
-		catch (BadFormatException e){
-			Logger.log("The specified audit file is not owned by the bot.", Logger.LogType.ERROR);
+		catch(IllegalAccessError e){
+			Logger.log("The specified audit file is not owned by the bot.",
+					Logger.LogType.ERROR);
 		}
-//		catch (FileNotFoundException e){
-//			Logger.log("The audit file does not exist and could not be created. Try to run the bot in admin mode.",Logger.LogType.ERROR);
-//		}
 		catch(IOException e){
-			if (!getFile().exists()){
-				Logger.log("The audit file does not exist and could not be created. Try to run the bot in admin mode.",Logger.LogType.ERROR);
+			
+			if(!auditFile.exists()){
+				Logger.log(
+						"The audit file does not exist and could not be created. Running the bot in admin mode might fix this.",
+						Logger.LogType.ERROR);
 			}
-			if(getFile().isDirectory()){
-				Logger.log("The location given for the audit is a directory.", Logger.LogType.ERROR );
+			else if(auditFile.isDirectory()){
+				Logger.log(
+						"The location given for the audit is a directory. Please go change the location in your code to point to a file.",
+						Logger.LogType.ERROR);
 			}
-			if (!getFile().canWrite()){
-				Logger.log("The file is not writable.", Logger.LogType.ERROR );
+			else if(!Files.isWritable(auditFile.toPath())){
+				Logger.log(
+						"The file is not writable. Running the bot in admin mode might fix this.",
+						Logger.LogType.ERROR);
+			}
+			else{
+				Logger.log(
+						"An unspecified error happened. Please try to run the bot again!",
+						Logger.LogType.ERROR);
 			}
 			
 		}
+		
+	}
+	
+	protected PrintWriter handleAuditHeader(File auditFile)
+			throws IllegalAccessError, IOException{
+		
+		boolean shouldWriteHeader = false;
+		
+		// If file doesn't exist, create it with a header.
+		if(!auditFile.exists()){
+			shouldWriteHeader = true;
+		}
+		else{
+			
+			BufferedReader reader = new BufferedReader(
+					new FileReader(auditFile));
+			String header = reader.readLine();
+			
+			// If file exists but is empty, we should write header to it.
+			if(header == null){
+				shouldWriteHeader = true;
+			}
+			else if(!header.equals(FrameworkResources.AUDIT_HEADER)){
+				throw new IllegalAccessError();
+			}
+			
+		}
+		
+		PrintWriter writer = new PrintWriter(new FileWriter(auditFile, true));
+		
+		if(shouldWriteHeader){
+			writer.println(FrameworkResources.AUDIT_HEADER + "\n");
+		}
+		
+		return writer;
 		
 	}
 	
