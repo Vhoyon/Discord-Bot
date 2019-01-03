0111bb2494733d0237db9cd7c8157dde7aebb737
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2019-01-02 22:27:43 -0500

    Comment out tests that are now supposed to be in Vramework

--- src/test/java/io/github/vhoyon/bot/utilities/BotCommandTest.java
+++ src/test/java/io/github/vhoyon/bot/utilities/BotCommandTest.java
@@ -1,24 +1,15 @@
 package io.github.vhoyon.bot.utilities;
 
-import io.github.vhoyon.bot.app.CommandRouter;
 import io.github.vhoyon.vramework.objects.Request;
 import io.github.vhoyon.vramework.utilities.formatting.DiscordFormatter;
-import io.github.vhoyon.vramework.utilities.settings.IntegerField;
-import io.github.vhoyon.vramework.utilities.settings.Setting;
-import io.github.vhoyon.vramework.utilities.settings.SettingRepository;
-import io.github.vhoyon.vramework.utilities.settings.TextField;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import utils.MockFactory;
 
-import java.util.concurrent.atomic.AtomicReference;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doReturn;
 
 class BotCommandTest implements DiscordFormatter {
@@ -79,78 +70,78 @@ class BotCommandTest implements DiscordFormatter {
 		assertEquals(expected, mockCommand.getUsage());
 	}
 	
-	@Test
-	void testGetSettingsNotChanged(){
-		String defValue = "defValue";
-		
-		Setting testField = new TextField("test", "ENV_TEST", defValue);
-		SettingRepository settings = new SettingRepository(testField);
-		
-		CommandRouter mockRouter = MockFactory.createRouter(settings);
-		
-		doReturn(mockRouter).when(mockCommand).getRouter();
-		
-		assertEquals(defValue, mockCommand.setting("test"));
-	}
-	
-	@Test
-	void testGetSettingsChanged(){
-		Setting testField = new TextField("test", "ENV_TEST", "testValue");
-		SettingRepository settings = new SettingRepository(testField);
-		
-		CommandRouter mockRouter = MockFactory.createRouter(settings);
-		
-		doReturn(mockRouter).when(mockCommand).getRouter();
-		
-		String newValue = "modifiedValue";
-		
-		mockCommand.setSetting("test", newValue);
-		
-		assertEquals(newValue, mockCommand.setting("test"));
-	}
-	
-	@Test
-	void testGetSettingsChangedCallback(){
-		Setting testField = new TextField("test", "ENV_TEST", "testValue");
-		SettingRepository settings = new SettingRepository(testField);
-		
-		CommandRouter mockRouter = MockFactory.createRouter(settings);
-		
-		doReturn(mockRouter).when(mockCommand).getRouter();
-		
-		AtomicReference<String> callbackedModifiedValue = new AtomicReference<>(
-				null);
-		
-		String newValue = "modifiedValue";
-		
-		mockCommand.setSetting("test", newValue,
-				(modifiedValue) -> callbackedModifiedValue
-						.set((String)modifiedValue));
-		
-		assertEquals(newValue, callbackedModifiedValue.get());
-	}
-	
-	@Test
-	void testGetSettingsAutoCast(){
-		Setting testFieldText = new TextField("testText", "ENV_TEST_TEXT",
-				"testValue");
-		Setting testFieldInt = new IntegerField("testInt", "ENV_TEST_INT",
-				2);
-		SettingRepository settings = new SettingRepository(testFieldText, testFieldInt);
-		
-		CommandRouter mockRouter = MockFactory.createRouter(settings);
-		
-		doReturn(mockRouter).when(mockCommand).getRouter();
-		
-		Executable shouldThrowClassCastException = () -> {
-			int badObject = mockCommand.setting("testText");
-		};
-		
-		assertThrows(ClassCastException.class, shouldThrowClassCastException);
-		
-		int goodObject = mockCommand.setting("testInt");
-		
-		assertEquals(2, goodObject);
-	}
+//	@Test
+//	void testGetSettingsNotChanged(){
+//		String defValue = "defValue";
+//		
+//		Setting testField = new TextField("test", "ENV_TEST", defValue);
+//		SettingRepository settings = new SettingRepository(testField);
+//		
+//		CommandRouter mockRouter = MockFactory.createRouter(settings);
+//		
+//		doReturn(mockRouter).when(mockCommand).getRouter();
+//		
+//		assertEquals(defValue, mockCommand.setting("test"));
+//	}
+//	
+//	@Test
+//	void testGetSettingsChanged(){
+//		Setting testField = new TextField("test", "ENV_TEST", "testValue");
+//		SettingRepository settings = new SettingRepository(testField);
+//		
+//		CommandRouter mockRouter = MockFactory.createRouter(settings);
+//		
+//		doReturn(mockRouter).when(mockCommand).getRouter();
+//		
+//		String newValue = "modifiedValue";
+//		
+//		mockCommand.setSetting("test", newValue);
+//		
+//		assertEquals(newValue, mockCommand.setting("test"));
+//	}
+//	
+//	@Test
+//	void testGetSettingsChangedCallback(){
+//		Setting testField = new TextField("test", "ENV_TEST", "testValue");
+//		SettingRepository settings = new SettingRepository(testField);
+//		
+//		CommandRouter mockRouter = MockFactory.createRouter(settings);
+//		
+//		doReturn(mockRouter).when(mockCommand).getRouter();
+//		
+//		AtomicReference<String> callbackedModifiedValue = new AtomicReference<>(
+//				null);
+//		
+//		String newValue = "modifiedValue";
+//		
+//		mockCommand.setSetting("test", newValue,
+//				(modifiedValue) -> callbackedModifiedValue
+//						.set((String)modifiedValue));
+//		
+//		assertEquals(newValue, callbackedModifiedValue.get());
+//	}
+//	
+//	@Test
+//	void testGetSettingsAutoCast(){
+//		Setting testFieldText = new TextField("testText", "ENV_TEST_TEXT",
+//				"testValue");
+//		Setting testFieldInt = new IntegerField("testInt", "ENV_TEST_INT",
+//				2);
+//		SettingRepository settings = new SettingRepository(testFieldText, testFieldInt);
+//		
+//		CommandRouter mockRouter = MockFactory.createRouter(settings);
+//		
+//		doReturn(mockRouter).when(mockCommand).getRouter();
+//		
+//		Executable shouldThrowClassCastException = () -> {
+//			int badObject = mockCommand.setting("testText");
+//		};
+//		
+//		assertThrows(ClassCastException.class, shouldThrowClassCastException);
+//		
+//		int goodObject = mockCommand.setting("testInt");
+//		
+//		assertEquals(2, goodObject);
+//	}
 	
 }
