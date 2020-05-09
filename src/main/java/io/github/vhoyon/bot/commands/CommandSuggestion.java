53c07509566fff258fbad2b88174ed7c6b1dcd3b
Stephano <stephanomehawej10@hotmail.com>
2020-05-08 22:28:13 -0400

    Add the Command suggestion

--- src/main/java/io/github/vhoyon/bot/commands/CommandSuggestion.java
+++ src/main/java/io/github/vhoyon/bot/commands/CommandSuggestion.java
@@ -1,11 +1,12 @@
-package java.io.github.vhoyon.bot.commands;
+package io.github.vhoyon.bot.commands;
 
 import io.github.stephanomehawej.gitinteractions.Issue;
+import io.github.stephanomehawej.gitinteractions.Management;
 import io.github.stephanomehawej.gitinteractions.exceptions.attributeNotFoundException;
 import io.github.stephanomehawej.gitinteractions.exceptions.invalidValueException;
+import io.github.vhoyon.bot.errorHandling.BotError;
 import io.github.vhoyon.bot.utilities.BotCommand;
-import org.json.JSONObject;
-import org.json.JSONTokener;
+
 
 import java.io.IOException;
 import java.net.URI;
@@ -17,25 +18,30 @@ public class CommandSuggestion extends BotCommand {
 	public void actions() {
 		Issue issue = null;
 
-		try {
-			
-			URI uri = new URI("");
-			JSONTokener tokener = new JSONTokener(uri.toURL().openStream());
-			JSONObject issueInfo = new JSONObject(tokener);
+		if(!hasContent()){
+			new BotError(lang("ContentMissing"));
+		}else{
+			try {
+				issue = new Issue(env("SUGGESTION_GITHUB_REPO"),env("SUGGESTION_GITHUB_LOGIN"),env("SUGGESTION_GITHUB_OAUTH"));
+				issue.setAttribue("title",lang( "Title" ,new Management().countByLabel("suggestions")+1));
+				issue.setAttribue("body",getContent());
+				issue.setAttribue("labels", new String[]{"suggestions"});
+				String url = issue.sendIssue();
+				
+				sendInfoMessage(lang("SentSuggestion", url));
 
+			} catch (IOException e) {
+				new BotError(this,e.getMessage());
+				e.printStackTrace();
+			} catch (invalidValueException e) {
+				new BotError(this,e.getMessage());
+				e.printStackTrace();
+			} catch (Exception e){
+				new BotError(this,e.getMessage());
 
-			issue = new Issue(issueInfo.get("repo").toString(),issueInfo.get("login").toString(),issueInfo.get("OAuth").toString());
-			issue.JSonIssue(issueInfo);
-			issue.sendIssue();
-		} catch (attributeNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (invalidValueException e) {
-			e.printStackTrace();
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
+			}
 		}
+		
 	}
 	@Override
 	public String getCall(){
